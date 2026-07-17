1ee9d3f742b15367f71c6820cde364de25040afc
shreyahtech <shreyaa.h553@gmail.com>
2026-07-17 18:10:47 +0530

    themes

--- components/dashboard.py
+++ components/dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-# Function to load CSS
+
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(
@@ -8,17 +8,35 @@ def local_css(file_name):
             unsafe_allow_html=True
         )
 
+
 def show_dashboard():
+
     st.set_page_config(
         page_title="AI Resume Screener Pro",
         page_icon="🤖",
-        layout="wide"
+        layout="wide",
+        initial_sidebar_state="expanded",
     )
 
-    # Load the CSS file
     local_css("assets/style.css")
 
-    st.title("🤖 AI Resume Screener Pro")
-    st.caption("Helping Recruiters Hire Smarter")
+    st.markdown(
+        """
+<div class="hero">
 
-    st.markdown("---")
+    <div class="hero-title">
+        🤖 AI Resume Screener Pro
+    </div>
+
+    <div class="hero-subtitle">
+        AI-Powered Recruitment Dashboard
+    </div>
+
+    <div class="hero-text">
+        Semantic Matching • ATS Analysis • AI Feedback • Resume Intelligence
+    </div>
+
+</div>
+        """,
+        unsafe_allow_html=True,
+    )
